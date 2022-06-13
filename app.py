cccd2ddaa23e7afe34d609891e8a33394906b81a
Muralidharbhusal <muralibhusal@gmail.com>
2022-06-13 23:13:13 +0545

    Initial Commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from camera import VideoTransformer
+from model import face_detect
+from homepage import home
+import streamlit as st
+from PIL import Image
+import numpy as np
+from streamlit_webrtc import webrtc_streamer
+
+
+def main():
+
+    #Title of the web app
+    st.title("Face Emotion detection")
+    
+    #Available function in the web app
+    activities = ["Homepage", "Deep learning with webcam", "Deep learning with picture"]
+    choice = st.sidebar.selectbox("Select the required activty to navigate.", activities)
+
+    #Navigating to Homepage
+    if choice == "Homepage":
+      
+      home()
+
+    #Navigating to face emotion detction with pre=trained deep learning model on live web-cam            
+    if choice == "Deep learning with webcam":
+
+      st.text('Select the respective devices for webcam and microphone(Allow access to camera and microphone if asked).')
+      st.text('Press START to start face emotion detection and STOP for stopping the detection.')
+      st.text('If the webcam is not opening then, Try pressing START and STOP and relaunching the tab sometimes as it may take some time to open the webcam in some cases. ')
+      webrtc_streamer(key="exam", video_transformer_factory=VideoTransformer)
+
+    #Navigating to face emotion detction with pre=trained deep learning model on picture            
+    if choice == "Deep learning with picture":
+
+      #Uploading the image
+      image_file = st.file_uploader("Upload the image", type=['jpeg', 'png', 'jpg'])
+
+      if image_file is not None:
+          image1 = Image.open(image_file)
+          image = np.asarray(image1)
+
+          #Displaying the uploaded image
+          st.image(image1)
+          st.text('Press Process to display the face emotion detected image.')
+
+          #Processing the uploaded image
+          if st.button("Process"):
+            face_detect(image)
+
+
+if __name__ == "__main__":
+    main()
---
+++ homepage.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+#Contents displayed on the Homepage
+def home():
+    st.write("This is an face emotion detection app that detects facial emotions.")
+    st.write("It can detect 7 emotions.")
+    st.write('Angry :angry:, Disgust :persevere:, Fear 😨, Happy :smile:, Sad :worried:, Suprise :astonished:, Neutral :expressionless:.')
+    st.subheader('Deep learning with Webcam and Deep learning with picture-')
+    st.text('They both use deep learning model to predict the face emotion in picture and in live webcam.')
+    st.text('You can use the deep learning with webcam to detect the facial emotion on real time through your webcam.')
+    st.text('You can use the deep learning with picture to detect facial emotion on the uploaded picture.')
